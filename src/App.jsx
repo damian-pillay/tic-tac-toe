1c51dbad15270ba285ede94c29f1ede8b26e6849
Damian <damian.pillay@kcpivot.co.za>
2025-04-23 12:55:01 +0200

    feat: lift state of player of player turn logic to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,48 @@ import { useState } from "react";
 import Log from "./components/Log";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
 
-  function squareSelectHandler() {
-    setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
+  function squareSelectHandler(rowIndex, colIndex) {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+    setGameTurns((prevTurns) => {
+      let currentPlayer =
+        (prevTurns.length > 0 && prevTurns[0].player) === "X" ? "O" : "X";
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={squareSelectHandler} activePlayerSymbol={activePlayer}/>
+        <GameBoard
+          onSelectSquare={squareSelectHandler}
+          activePlayerSymbol={activePlayer}
+          turns={gameTurns}
+        />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
