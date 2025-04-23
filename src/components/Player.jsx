cc82e5dd1e09805a2c0f0b1279834e1f2c431913
Damian <damian.pillay@kcpivot.co.za>
2025-04-23 10:16:01 +0200

    feat: use 2 way binding to allow text inputs to update player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function editHandler() {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
+  }
+
+  function inputHandler(event) {
+    setPlayerName(event.target.value);
   }
 
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required></input>
+          <input type="text" required value={playerName} onChange={inputHandler}></input>
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
